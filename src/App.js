6a0d889ab9e265b85fab54bed9b50c6c9eace0e0
Leocoelho19822 <leo.coelho@ua.pt>
2024-02-22 22:44:12 +0000

    criação do header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header/Header';
 import Homepage from './homepage';
 
 function App() {
   return (
     <div>
+      <Header />
       <Homepage/>
     </div>
   );
